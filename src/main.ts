7720ded6a7c83e7c076f3af427d47f0097fcb405
Ngoc <cercesama@gmail.com>
2026-09-08 07:39:21 +0700

    setup database - connect to neon

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   })
+  app.setGlobalPrefix('api/v1')
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
 
   const config = new DocumentBuilder()
     .setTitle('Writing API')
