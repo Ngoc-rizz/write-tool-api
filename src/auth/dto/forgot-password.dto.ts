75c14a89f848c72fd5ebc41c417620d2e2b173f0
Ngoc <cercesama@gmail.com>
2026-09-12 11:11:18 +0700

    add regex

--- src/auth/dto/forgot-password.dto.ts
+++ src/auth/dto/forgot-password.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, Matches } from "class-validator";
 
 export class ForgotPasswordDTO {
     @ApiProperty()
     @IsEmail()
     @IsNotEmpty()
+    @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+        message: 'Email is not in a valid format',
+    })
     email: string
 }
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,16 @@
 import {
     IsEmail,
     IsString,
+    Matches,
     MaxLength,
     MinLength,
 } from 'class-validator';
 
 export class LoginDto {
     @IsEmail()
+    @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+        message: 'Email is not in a valid format',
+    })
     email: string;
 
     @IsString()
